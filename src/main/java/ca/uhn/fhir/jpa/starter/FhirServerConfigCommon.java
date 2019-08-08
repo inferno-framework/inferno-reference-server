bb97abbe220b7ec0c919f49943ce32174d968a19
Hershil Patel <hershil@mitre.org>
2019-08-08 17:59:54 -0400

    initial commit, started with as a copy of https://github.com/hapifhir/hapi-fhir-jpaserver-starter

---
+++ src/main/java/ca/uhn/fhir/jpa/starter/FhirServerConfigCommon.java
@@ -0,0 +1,171 @@
+package ca.uhn.fhir.jpa.starter;
+
+import ca.uhn.fhir.jpa.dao.DaoConfig;
+import ca.uhn.fhir.jpa.model.entity.ModelConfig;
+import ca.uhn.fhir.jpa.subscription.module.cache.SubscriptionDeliveryHandlerFactory;
+import ca.uhn.fhir.jpa.subscription.module.subscriber.email.IEmailSender;
+import ca.uhn.fhir.jpa.subscription.module.subscriber.email.JavaMailEmailSender;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hl7.fhir.instance.model.Subscription;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.thymeleaf.util.Validate;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Driver;
+
+/**
+ * This is the primary configuration file for the example server
+ */
+@Configuration
+@EnableTransactionManagement()
+public class FhirServerConfigCommon {
+
+    private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(FhirServerConfigCommon.class);
+
+    private Boolean allowContainsSearches = HapiProperties.getAllowContainsSearches();
+    private Boolean allowMultipleDelete = HapiProperties.getAllowMultipleDelete();
+    private Boolean allowExternalReferences = HapiProperties.getAllowExternalReferences();
+    private Boolean expungeEnabled = HapiProperties.getExpungeEnabled();
+    private Boolean allowPlaceholderReferences = HapiProperties.getAllowPlaceholderReferences();
+    private Boolean subscriptionRestHookEnabled = HapiProperties.getSubscriptionRestHookEnabled();
+    private Boolean subscriptionEmailEnabled = HapiProperties.getSubscriptionEmailEnabled();
+    private Boolean allowOverrideDefaultSearchParams = HapiProperties.getAllowOverrideDefaultSearchParams();
+    private String emailFrom = HapiProperties.getEmailFrom();
+    private Boolean emailEnabled = HapiProperties.getEmailEnabled();
+    private String emailHost = HapiProperties.getEmailHost();
+    private Integer emailPort = HapiProperties.getEmailPort();
+    private String emailUsername = HapiProperties.getEmailUsername();
+    private String emailPassword = HapiProperties.getEmailPassword();
+    @Autowired
+    private SubscriptionDeliveryHandlerFactory mySubscriptionDeliveryHandlerFactory;
+
+    public FhirServerConfigCommon() {
+        ourLog.info("Server configured to " + (this.allowContainsSearches ? "allow" : "deny") + " contains searches");
+        ourLog.info("Server configured to " + (this.allowMultipleDelete ? "allow" : "deny") + " multiple deletes");
+        ourLog.info("Server configured to " + (this.allowExternalReferences ? "allow" : "deny") + " external references");
+        ourLog.info("Server configured to " + (this.expungeEnabled ? "enable" : "disable") + " expunges");
+        ourLog.info("Server configured to " + (this.allowPlaceholderReferences ? "allow" : "deny") + " placeholder references");
+        ourLog.info("Server configured to " + (this.allowOverrideDefaultSearchParams ? "allow" : "deny") + " overriding default search params");
+
+        if (this.emailEnabled) {
+            ourLog.info("Server is configured to enable email with host '" + this.emailHost + "' and port " + this.emailPort.toString());
+            ourLog.info("Server will use '" + this.emailFrom + "' as the from email address");
+
+            if (this.emailUsername != null && this.emailUsername.length() > 0) {
+                ourLog.info("Server is configured to use username '" + this.emailUsername + "' for email");
+            }
+
+            if (this.emailPassword != null && this.emailPassword.length() > 0) {
+                ourLog.info("Server is configured to use a password for email");
+            }
+        }
+
+        if (this.subscriptionRestHookEnabled) {
+            ourLog.info("REST-hook subscriptions enabled");
+        }
+
+        if (this.subscriptionEmailEnabled) {
+            ourLog.info("Email subscriptions enabled");
+        }
+    }
+
+    /**
+     * Configure FHIR properties around the the JPA server via this bean
+     */
+    @Bean()
+    public DaoConfig daoConfig() {
+        DaoConfig retVal = new DaoConfig();
+
+        retVal.setAllowContainsSearches(this.allowContainsSearches);
+        retVal.setAllowMultipleDelete(this.allowMultipleDelete);
+        retVal.setAllowExternalReferences(this.allowExternalReferences);
+        retVal.setExpungeEnabled(this.expungeEnabled);
+        retVal.setAutoCreatePlaceholderReferenceTargets(this.allowPlaceholderReferences);
+        retVal.setEmailFromAddress(this.emailFrom);
+
+        Integer maxFetchSize = HapiProperties.getMaximumFetchSize();
+        retVal.setFetchSizeDefaultMaximum(maxFetchSize);
+        ourLog.info("Server configured to have a maximum fetch size of " + (maxFetchSize == Integer.MAX_VALUE ? "'unlimited'" : maxFetchSize));
+
+        Long reuseCachedSearchResultsMillis = HapiProperties.getReuseCachedSearchResultsMillis();
+        retVal.setReuseCachedSearchResultsForMillis(reuseCachedSearchResultsMillis);
+        ourLog.info("Server configured to cache search results for {} milliseconds", reuseCachedSearchResultsMillis);
+
+        // Subscriptions are enabled by channel type
+        if (HapiProperties.getSubscriptionRestHookEnabled()) {
+            ourLog.info("Enabling REST-hook subscriptions");
+            retVal.addSupportedSubscriptionType(Subscription.SubscriptionChannelType.RESTHOOK);
+        }
+        if (HapiProperties.getSubscriptionEmailEnabled()) {
+            ourLog.info("Enabling email subscriptions");
+            retVal.addSupportedSubscriptionType(Subscription.SubscriptionChannelType.EMAIL);
+        }
+        if (HapiProperties.getSubscriptionWebsocketEnabled()) {
+            ourLog.info("Enabling websocket subscriptions");
+            retVal.addSupportedSubscriptionType(Subscription.SubscriptionChannelType.WEBSOCKET);
+        }
+
+        return retVal;
+    }
+
+    @Bean
+    public ModelConfig modelConfig() {
+        ModelConfig modelConfig = new ModelConfig();
+        modelConfig.setAllowContainsSearches(this.allowContainsSearches);
+        modelConfig.setAllowExternalReferences(this.allowExternalReferences);
+        modelConfig.setDefaultSearchParamsCanBeOverridden(this.allowOverrideDefaultSearchParams);
+        modelConfig.setEmailFromAddress(this.emailFrom);
+
+        // You can enable these if you want to support Subscriptions from your server
+        if (this.subscriptionRestHookEnabled) {
+            modelConfig.addSupportedSubscriptionType(Subscription.SubscriptionChannelType.RESTHOOK);
+        }
+
+        if (this.subscriptionEmailEnabled) {
+            modelConfig.addSupportedSubscriptionType(Subscription.SubscriptionChannelType.EMAIL);
+        }
+
+        return modelConfig;
+    }
+
+    /**
+     * The following bean configures the database connection. The 'url' property value of "jdbc:derby:directory:jpaserver_derby_files;create=true" indicates that the server should save resources in a
+     * directory called "jpaserver_derby_files".
+     * <p>
+     * A URL to a remote database could also be placed here, along with login credentials and other properties supported by BasicDataSource.
+     */
+    @Bean(destroyMethod = "close")
+    public BasicDataSource dataSource() throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        BasicDataSource retVal = new BasicDataSource();
+        Driver driver = (Driver) Class.forName(HapiProperties.getDataSourceDriver()).getConstructor().newInstance();
+        retVal.setDriver(driver);
+        retVal.setUrl(HapiProperties.getDataSourceUrl());
+        retVal.setUsername(HapiProperties.getDataSourceUsername());
+        retVal.setPassword(HapiProperties.getDataSourcePassword());
+        retVal.setMaxTotal(HapiProperties.getDataSourceMaxPoolSize());
+        return retVal;
+    }
+
+    @Bean()
+    public IEmailSender emailSender() {
+        if (this.emailEnabled) {
+            JavaMailEmailSender retVal = new JavaMailEmailSender();
+
+            retVal.setSmtpServerHostname(this.emailHost);
+            retVal.setSmtpServerPort(this.emailPort);
+            retVal.setSmtpServerUsername(this.emailUsername);
+            retVal.setSmtpServerPassword(this.emailPassword);
+
+            Validate.notNull(mySubscriptionDeliveryHandlerFactory, "No subscription delivery handler");
+            mySubscriptionDeliveryHandlerFactory.setEmailSender(retVal);
+
+
+            return retVal;
+        }
+
+        return null;
+    }
+}
---
+++ src/test/java/ca/uhn/fhir/jpa/starter/ExampleServerDstu2IT.java
@@ -0,0 +1,84 @@
+package ca.uhn.fhir.jpa.starter;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.model.dstu2.resource.Patient;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.rest.client.api.ServerValidationModeEnum;
+import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
+import ca.uhn.fhir.util.PortUtil;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.hl7.fhir.instance.model.api.IIdType;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleServerDstu2IT {
+
+	private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(ExampleServerDstu2IT.class);
+	private static IGenericClient ourClient;
+	private static FhirContext ourCtx;
+	private static int ourPort;
+
+	private static Server ourServer;
+	private static String ourServerBase;
+
+	static {
+		HapiProperties.forceReload();
+		HapiProperties.setProperty(HapiProperties.FHIR_VERSION, "DSTU2");
+		HapiProperties.setProperty(HapiProperties.DATASOURCE_URL, "jdbc:derby:memory:dbr2;create=true");
+		ourCtx = FhirContext.forDstu2();
+		ourPort = PortUtil.findFreePort();
+	}
+
+	@Test
+	public void testCreateAndRead() {
+		ourLog.info("Base URL is: " +  HapiProperties.getServerAddress());
+		String methodName = "testCreateResourceConditional";
+
+		Patient pt = new Patient();
+		pt.addName().addFamily(methodName);
+		IIdType id = ourClient.create().resource(pt).execute().getId();
+
+		Patient pt2 = ourClient.read().resource(Patient.class).withId(id).execute();
+		assertEquals(methodName, pt2.getName().get(0).getFamily().get(0).getValue());
+	}
+
+	@AfterClass
+	public static void afterClass() throws Exception {
+		ourServer.stop();
+	}
+
+	@BeforeClass
+	public static void beforeClass() throws Exception {
+		String path = Paths.get("").toAbsolutePath().toString();
+
+		ourLog.info("Project base path is: {}", path);
+
+		ourServer = new Server(ourPort);
+
+		WebAppContext webAppContext = new WebAppContext();
+		webAppContext.setContextPath("/hapi-fhir-jpaserver");
+		webAppContext.setDescriptor(path + "/src/main/webapp/WEB-INF/web.xml");
+		webAppContext.setResourceBase(path + "/target/hapi-fhir-jpaserver-starter");
+		webAppContext.setParentLoaderPriority(true);
+
+		ourServer.setHandler(webAppContext);
+		ourServer.start();
+
+		ourCtx.getRestfulClientFactory().setServerValidationMode(ServerValidationModeEnum.NEVER);
+		ourCtx.getRestfulClientFactory().setSocketTimeout(1200 * 1000);
+		ourServerBase = "http://localhost:" + ourPort + "/hapi-fhir-jpaserver/fhir/";
+		ourClient = ourCtx.newRestfulGenericClient(ourServerBase);
+		ourClient.registerInterceptor(new LoggingInterceptor(true));
+	}
+
+	public static void main(String[] theArgs) throws Exception {
+		ourPort = 8080;
+		beforeClass();
+	}
+}
