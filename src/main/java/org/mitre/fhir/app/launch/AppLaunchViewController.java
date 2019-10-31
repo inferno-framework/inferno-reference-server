7bb0bfca776b4da16a4d4718b5c9b0a9dffe7f28
Hershil Patel <hershil@mitre.org>
2019-10-31 12:16:02 -0400

    added support for an app launch for the inferno practitioner test ELS-01

---
+++ src/main/java/org/mitre/fhir/app/launch/AppLaunchViewController.java
@@ -0,0 +1,17 @@
+package org.mitre.fhir.app.launch;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+ * Controller that serves the src/main/webapp/WEB-INF/templates/app-launch.html template 
+ */
+@Controller
+public class AppLaunchViewController {
+
+	@RequestMapping("/app-launch")
+	public String showAppLaunchView() {
+		return "app-launch"; // String will be mapped to corresponding template html file
+	}
+
+}
