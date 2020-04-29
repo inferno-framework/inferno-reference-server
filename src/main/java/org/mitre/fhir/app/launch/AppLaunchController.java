3dcdef44713b1a7c6b65d95efb970673fdd26f0e
Reece Adamson <radamson@mitre.org>
2020-04-29 18:42:36 -0400

    fix AppLaunchController

--- src/main/java/org/mitre/fhir/app/launch/AppLaunchController.java
+++ src/main/java/org/mitre/fhir/app/launch/AppLaunchController.java
@@ -1,5 +1,6 @@
 package org.mitre.fhir.app.launch;
 
+import javax.servlet.http.HttpServletRequest;
 import org.json.JSONObject;
 import org.mitre.fhir.utils.FhirReferenceServerUtils;
 import org.mitre.fhir.utils.exception.RSAKeyException;
@@ -8,20 +9,32 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-
 @RestController
 public class AppLaunchController {
 
+  /**
+   * Provides the base url of the FHIR Server.
+   *
+   * @param request the incoming HTTP Request
+   * @return String the base url of the FHIR Server
+   */
   @GetMapping(path = "/fhir-server-path")
   public ResponseEntity<String> getFhirServerPath(HttpServletRequest request) {
     String fhirServerBaseUrl = FhirReferenceServerUtils.getFhirServerBaseUrl(request);
-    ResponseEntity<String> responseEntity = new ResponseEntity<String>(fhirServerBaseUrl, HttpStatus.OK);
+    ResponseEntity<String> responseEntity = new ResponseEntity<String>(fhirServerBaseUrl,
+        HttpStatus.OK);
     return responseEntity;
   }
 
+  /**
+   * Provides the SMART App Styling
+   * http://www.hl7.org/fhir/smart-app-launch/scopes-and-launch-context/#styling
+   *
+   * @param theRequest the incoming HTTP Request
+   * @return String the JSON SMART Styling response
+   */
   @GetMapping(path = "/smart-style-url", produces = {"application/json"})
-  public String getSmartStyleUrl(HttpServletRequest theRequest) throws RSAKeyException {
+  public String getSmartStyleUrl(HttpServletRequest theRequest) {
     //example json from http://www.hl7.org/fhir/smart-app-launch/scopes-and-launch-context/index.html#styling
     JSONObject json = new JSONObject();
     json.put("color_background", "#edeae3");
@@ -34,7 +47,8 @@ public class AppLaunchController {
     json.put("dim_font_size", "13px");
     json.put("dim_spacing_size", "20px");
     json.put("font_family_body", "Georgia, Times, 'Times New Roman', serif");
-    json.put("font_family_heading", "'HelveticaNeue-Light', Helvetica, Arial, 'Lucida Grande', sans-serif;");
+    json.put("font_family_heading",
+        "'HelveticaNeue-Light', Helvetica, Arial, 'Lucida Grande', sans-serif;");
     return json.toString();
   }
 }
