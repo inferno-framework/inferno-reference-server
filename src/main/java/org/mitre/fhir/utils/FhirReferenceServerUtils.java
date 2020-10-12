4235a0ef5a20f0a2245b4e58342cf21ee5cd2ff6
Hershil Patel <hershil@mitre.org>
2020-10-12 13:53:10 -0400

    added support for specifying a custom port in an env variable for port removal due to proxy issues

--- src/main/java/org/mitre/fhir/utils/FhirReferenceServerUtils.java
+++ src/main/java/org/mitre/fhir/utils/FhirReferenceServerUtils.java
@@ -20,6 +20,8 @@ public class FhirReferenceServerUtils {
 
   public static final String DEFAULT_SCOPE = "launch/patient patient/*";
 
+  public static final String CUSTOM_PORT_KEY = "CUSTOM_PORT";
+  
   private static final String HTTP = "http";
   private static final String HTTPS = "https";
   private static final int HTTP_DEFAULT_PORT = 80;
@@ -36,9 +38,19 @@ public class FhirReferenceServerUtils {
     int portNumber = request.getServerPort();
     String port = ":" + portNumber;
 
+    String customPortString = System.getenv(CUSTOM_PORT_KEY);
+
+    int customPortNumber = -1;
+    try {
+      customPortNumber = Integer.parseInt(customPortString);
+    } catch (NumberFormatException numberFormatException) {
+
+    }
+
     // if default port, remove the port
     if ((HTTP.equals(scheme) && portNumber == HTTP_DEFAULT_PORT)
-        || (HTTPS.equals(scheme) && portNumber == HTTPS_DEFAULT_PORT)) {
+        || (HTTPS.equals(scheme) && portNumber == HTTPS_DEFAULT_PORT)
+        || portNumber == customPortNumber) {
       port = "";
     }
 
