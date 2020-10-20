b0a6cb1fe27afb7077903d173d71496fda5c29c3
Hershil Patel <hershil@mitre.org>
2020-10-20 14:20:54 -0400

    undoing the custom deployment as it is no longer necessary

--- src/test/java/org/mitre/fhir/utils/TestFhirReferenceServerUtils.java
+++ src/test/java/org/mitre/fhir/utils/TestFhirReferenceServerUtils.java
@@ -1,6 +1,5 @@
 package org.mitre.fhir.utils;
 
-import com.github.stefanbirkner.systemlambda.SystemLambda;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -18,40 +17,6 @@ public class TestFhirReferenceServerUtils {
 
     String baseUrl = FhirReferenceServerUtils.getServerBaseUrl(mockHttpServletRequest);
     Assert.assertEquals("http://www.example.org:123/reference-server", baseUrl);
-
-  }
-
-  @Test
-  public void testGetServerBaseUrlWithEnvVar() throws Exception {
-    SystemLambda.withEnvironmentVariable("CUSTOM_DEPLOYMENT_PORT", "8443").execute(() -> {
-      MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
-
-      mockHttpServletRequest.setScheme("http");
-      mockHttpServletRequest.setServerName("www.example.org");
-      mockHttpServletRequest.setServerPort(8443);
-      mockHttpServletRequest.setRequestURI("/.well-known/smart-configuration");
-
-      String baseUrl = FhirReferenceServerUtils.getServerBaseUrl(mockHttpServletRequest);
-      Assert.assertEquals("http://www.example.org/reference-server", baseUrl);
-
-    });
-
-  }
-  
-  @Test
-  public void testGetServerBaseUrlWithEnvVarInvalidValue() throws Exception {
-    SystemLambda.withEnvironmentVariable("CUSTOM_DEPLOYMENT_PORT", "TEST").execute(() -> {
-      MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
-
-      mockHttpServletRequest.setScheme("http");
-      mockHttpServletRequest.setServerName("www.example.org");
-      mockHttpServletRequest.setServerPort(8443);
-      mockHttpServletRequest.setRequestURI("/.well-known/smart-configuration");
-
-      String baseUrl = FhirReferenceServerUtils.getServerBaseUrl(mockHttpServletRequest);
-      Assert.assertEquals("http://www.example.org:8443/reference-server", baseUrl);
-
-    });
 
   }
 
