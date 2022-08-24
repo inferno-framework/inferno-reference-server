5b030c5743825303e238f74e0594ec9044a4a877
Jack Fraser <37051655+Jack-Fraser16@users.noreply.github.com>
2022-08-24 16:19:35 -0700

    create unit tests for ReadOnlyInterceptor

---
+++ src/test/java/org/mitre/fhir/TestReadOnlyInterceptor.java
@@ -0,0 +1,130 @@
+package org.mitre.fhir;
+
+import org.junit.Test;
+import java.io.FileWriter;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.hl7.fhir.r4.model.IdType;
+import org.hl7.fhir.r4.model.Patient;
+import org.mitre.fhir.utils.TestUtils;
+import org.eclipse.jetty.server.Server;
+import ca.uhn.fhir.context.FhirContext;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.mitre.fhir.authorization.token.Token;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import org.mitre.fhir.utils.FhirReferenceServerUtils;
+import org.mitre.fhir.authorization.token.TokenManager;
+import ca.uhn.fhir.rest.client.api.ServerValidationModeEnum;
+import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
+import ca.uhn.fhir.rest.server.exceptions.MethodNotAllowedException;
+
+public class TestReadOnlyInterceptor {
+
+  private static int ourPort;
+  private static Token testToken;
+  private static Server ourServer;
+  private static FhirContext ourCtx;
+  private static String ourServerBase;
+  private static IGenericClient ourClient;
+  private static String ConfigFileContent;
+
+  @Test(expected = MethodNotAllowedException.class)
+  public void testReadOnlyPreventCreate() throws MethodNotAllowedException {
+    createPatient();
+  }
+
+  @Test(expected = MethodNotAllowedException.class)
+  public void testReadOnlyPreventUpdate() throws MethodNotAllowedException {
+    updatePatient();
+  }
+
+  @Test(expected = MethodNotAllowedException.class)
+  public void testReadOnlyPreventDelete() throws MethodNotAllowedException {
+    deletePatient();
+  }
+
+  private void createPatient() throws MethodNotAllowedException {
+    Patient pt = new Patient();
+    pt.addName().setFamily("Test");
+    ourClient.create().resource(pt)
+            .withAdditionalHeader(FhirReferenceServerUtils.AUTHORIZATION_HEADER_NAME,
+                    FhirReferenceServerUtils.createAuthorizationHeaderValue(testToken.getTokenValue()))
+            .execute().getId();
+  }
+
+  private void updatePatient() throws MethodNotAllowedException {
+    Patient pt = new Patient();
+    pt.addName().setFamily("Test");
+    pt.setId("1234");
+    ourClient.update().resource(pt)
+            .withAdditionalHeader(FhirReferenceServerUtils.AUTHORIZATION_HEADER_NAME,
+                    FhirReferenceServerUtils.createAuthorizationHeaderValue(testToken.getTokenValue()))
+            .execute().getId();
+  }
+
+  private void deletePatient() throws MethodNotAllowedException {
+    Patient pt = new Patient();
+    pt.addName().setFamily("Test");
+    pt.setId("Patient/1234");
+    ourClient.delete().resource(pt)
+            .withAdditionalHeader(FhirReferenceServerUtils.AUTHORIZATION_HEADER_NAME,
+                    FhirReferenceServerUtils.createAuthorizationHeaderValue(testToken.getTokenValue()))
+            .execute().getId();
+  }
+
+  public static String CopyConfig() throws Exception {
+    Path FilePath = Path.of("src/config.properties");
+    return Files.readString(FilePath);
+  }
+
+  public static void OverWriteConfig(String ContentToWrite) throws Exception {
+    FileWriter ConfigWriter = new FileWriter("src/config.properties");
+    ConfigWriter.write("");
+    ConfigWriter.write(ContentToWrite);
+    ConfigWriter.close();
+  }
+
+  @BeforeClass
+  public static void beforeClass() throws Exception {
+
+    ConfigFileContent = CopyConfig();
+    OverWriteConfig("READ_ONLY=true");
+
+    testToken = TokenManager.getInstance().getServerToken();
+    ourCtx = FhirContext.forR4();
+
+    if (ourPort == 0) { ourPort = TestUtils.TEST_PORT; }
+
+    ourServer = new Server(ourPort);
+
+    String path = Paths.get("").toAbsolutePath().toString();
+
+    WebAppContext webAppContext = new WebAppContext();
+    webAppContext.setContextPath("");
+    webAppContext.setDisplayName("HAPI FHIR");
+    webAppContext.setDescriptor(path + "/src/main/webapp/WEB-INF/web.xml");
+    webAppContext.setResourceBase(path + "/target/mitre-fhir-starter");
+    webAppContext.setParentLoaderPriority(true);
+
+    ourServer.setHandler(webAppContext);
+    ourServer.start();
+
+    ourCtx.getRestfulClientFactory().setServerValidationMode(ServerValidationModeEnum.NEVER);
+    ourCtx.getRestfulClientFactory().setSocketTimeout(1200 * 1000);
+    ourServerBase = "http://localhost:" + ourPort + "/reference-server/r4/";
+
+    ourClient = ourCtx.newRestfulGenericClient(ourServerBase);
+    ourClient.registerInterceptor(new LoggingInterceptor(true));
+    ourClient.capabilities();
+  }
+
+  @AfterClass
+  public static void afterClass() throws Exception {
+    OverWriteConfig(ConfigFileContent);
+    ourServer.stop();
+  }
+}
+
