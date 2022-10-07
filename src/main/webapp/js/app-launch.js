d0691ac483b35f349cdfc2c047169c7ede4428ce
Jack Fraser <37051655+Jack-Fraser16@users.noreply.github.com>
2022-10-06 22:06:29 -0700

    create .js file for app-launch html

---
+++ src/main/webapp/js/app-launch.js
@@ -0,0 +1,10 @@
+window.mitre = window.mitre || {};
+window.mitre.fhirreferenceserver = window.mitre.fhirreferenceserver || {};
+window.mitre.fhirreferenceserver.appLaunch = {
+    /**
+     * Initializes the page and all html components including actions
+     */
+     init: function () {
+
+     }
+}
