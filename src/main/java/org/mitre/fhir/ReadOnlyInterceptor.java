369af5c02e0d82f10311e84b193334e1dcb1bc76
Jack Fraser <37051655+Jack-Fraser16@users.noreply.github.com>
2022-10-24 21:51:05 -0700

    add EXTEND_OPERATION_SERVER

--- src/main/java/org/mitre/fhir/ReadOnlyInterceptor.java
+++ src/main/java/org/mitre/fhir/ReadOnlyInterceptor.java
@@ -27,7 +27,8 @@ public class ReadOnlyInterceptor {
             && theOperation != RestOperationTypeEnum.VALIDATE
             && theOperation != RestOperationTypeEnum.VREAD
             && theOperation != RestOperationTypeEnum.GET_PAGE
-            && theOperation != RestOperationTypeEnum.EXTENDED_OPERATION_INSTANCE) {
+            && theOperation != RestOperationTypeEnum.EXTENDED_OPERATION_INSTANCE
+            && theOperation != RestOperationTypeEnum.EXTENDED_OPERATION_SERVER) {
       throw new MethodNotAllowedException("Server is currently `read-only`: the "
               + theOperation.toString()
               + " operation is not allowed.");
