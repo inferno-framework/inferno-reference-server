2a1dcd553003321384a0cd6368314d0779231995
Dylan Hall <dehall@mitre.org>
2024-10-28 08:36:11 -0400

    FI-3258: Add CORS support per SMART 2.2 (#176)

--- src/main/java/org/mitre/fhir/authorization/token/TokenController.java
+++ src/main/java/org/mitre/fhir/authorization/token/TokenController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.json.JSONObject;
 import org.mitre.fhir.utils.FhirReferenceServerUtils;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/token")
+@CrossOrigin(originPatterns = {"*"}, exposedHeaders = {"*"}, allowCredentials = "true")
 public class TokenController {
   /**
    * Service to revoke a token.
