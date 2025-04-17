5ff71063984d3525b3702fb60643fe02d40721a9
Dylan Hall <dehall@mitre.org>
2025-04-17 08:08:03 -0400

    fix scripts

--- scripts/start_reference_server.sh
+++ scripts/start_reference_server.sh
@@ -2,7 +2,7 @@
 # This script is intended to be called from Docker to perform any needed
 # initialization before launching the jetty server.
  
-./fetch_data_repo.sh
+scripts/fetch_data_repo.sh
 
 # Run the same command as the original Jetty image to start the server, see:
 # https://github.com/jetty/jetty.docker/blob/master/baseDockerfile
